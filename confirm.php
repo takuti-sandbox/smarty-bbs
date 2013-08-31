b7a07567a9cba2b127e9882bb87a7ce3719c845f
takuti <k.takuti@gmail.com>
2013-08-31 16:38:51 +0900

    escape

--- confirm.php
+++ confirm.php
@@ -8,9 +8,10 @@ $smarty->assign('post_action', 'post.php');
 $smarty->assign('edit_action', 'index.php');
 
 if(@$_POST['reply_to']) {
-	$smarty->assign('reply_to', $_POST['reply_to']);
+	$reply_to = htmlspecialchars($_POST['reply_to']);
+	$smarty->assign('reply_to', $reply_to);
 	$smarty->assign('post_action', 'reply.php');
-	$smarty->assign('edit_action', 'single.php?post='.$_POST['reply_to']);
+	$smarty->assign('edit_action', 'single.php?post='.$reply_to);
 }
 
 $smarty->display('confirm.tpl');
--- reply.php
+++ reply.php
@@ -5,7 +5,7 @@ $pdo = new PDO("mysql:dbname=bbs", "root", "root");
 $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
 $title = htmlspecialchars($_POST['title'], ENT_QUOTES, 'UTF-8');
 $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
-$reply_to = $_POST['reply_to'];
+$reply_to = htmlspecialchars($_POST['reply_to']);
 
 $st = $pdo->prepare("INSERT INTO reply(name,title,message,reply_to) VALUES(?,?,?,?)");
 $st->execute(array($name, $title, $message, $reply_to));
